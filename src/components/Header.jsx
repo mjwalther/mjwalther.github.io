e087039658137878cccf14ecbe99cca6500f44e4
jirowalther <93296324+jirowalther@users.noreply.github.com>
2025-12-12 01:45:16 -0800

    Open social links in new tab

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,8 +13,8 @@ export const Header = () => {
                 <h2 style={headerStyle}>Data and Computer Science @ Stanford University</h2>
                 <h3 style={headerStyle}>jiro /at/ stanford.edu</h3>
                 <div style={{display: "flex", flexDirection: "row"}}>
-                    <a href="https://github.com/mjwalther" aria-label="Github Link" ><FaGithub style={{marginRight: "5px", fontSize:"xx-large", color: "#6abe83"}}/></a>
-                    <a href="https://www.linkedin.com/in/jirowalther/" aria-label="LinkedIn Link"><FaLinkedin style={{marginRight: "5px", fontSize:"xx-large", color: "#84c9fb"}}/></a>                </div>
+                    <a href="https://github.com/mjwalther" aria-label="Github Link" target="_blank" rel="noopener noreferrer"><FaGithub style={{marginRight: "5px", fontSize:"xx-large", color: "#6abe83"}}/></a>
+                    <a href="https://www.linkedin.com/in/jirowalther/" aria-label="LinkedIn Link" target="_blank" rel="noopener noreferrer"><FaLinkedin style={{marginRight: "5px", fontSize:"xx-large", color: "#84c9fb"}}/></a>                </div>
             </div>
         </div>
     )
